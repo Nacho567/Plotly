861506dd5a2604d9af3abb0f7844f473cdd9e6d6
Ethan N <ethan.norberg@gmail.com>
2023-02-18 14:16:41 -0600

    added class exercises to repo

---
+++ 13-Plotly/2/Activities/02-Stu_Multi_Trace/Unsolved/plots.js
@@ -0,0 +1,33 @@
+console.log(data);
+
+// var sortedByGreekSearch = data.sort((a, b) => b.greekSearchResults - a.greekSearchResults);
+
+// Trace1 for the Greek Data
+var trace1 = {
+    x: data.map(row => row.pair),
+    y: data.map(row => row.greekSearchResults),
+    text: data.map(row => row.greekName),
+    name: "Greek",
+    type: "bar"
+};
+
+// Trace 2 for the Roman Data
+var trace2 = {
+    x: data.map(row => row.pair),
+    y: data.map(row => row.romanSearchResults),
+    text: data.map(row => row.romanName),
+    name: "Roman",
+    type: "bar"
+};
+
+// Combining both traces
+var data2 = [trace1, trace2];
+
+// Apply the group barmode to the layout
+var layout = {
+    title: "Greek Gods Popularity",
+    barmode: "group"
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data2, layout);
---
+++ 13-Plotly/2/Activities/05-Stu-Event_Final/Unsolved/plots.js
@@ -0,0 +1,53 @@
+// Create an array of each country's numbers
+var us = Object.values(data.us);
+var uk = Object.values(data.uk);
+var canada = Object.values(data.canada);
+
+// Create an array of music provider labels
+var labels = Object.keys(data.us);
+
+// Display the default plot
+function init() {
+  var data = [{
+    values: us,
+    labels: labels,
+    type: "pie",
+    hole: 0.4
+  }];
+
+  var layout = {
+    height: 600,
+    width: 800
+  };
+
+  Plotly.newPlot("pie", data, layout);
+}
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  var dataset = dropdownMenu.property("value");
+  // Initialize an empty array for the country's data
+  var data = [];
+
+  if (dataset == 'us') {
+    data = us;
+  }
+  else if (dataset == 'uk') {
+    data = uk;
+  }
+  else if (dataset == 'canada') {
+    data = canada;
+  }
+
+updatePlotly(data);
+}
+// Update the restyled plot's values
+function updatePlotly(newData) {
+  Plotly.restyle("pie", "values", [newData]);
+}
+init();
